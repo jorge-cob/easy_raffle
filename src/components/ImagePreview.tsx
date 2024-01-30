83db061b83584aad1d7c8721d5a22c3ce7752d35
grisley <grisley@debian.tecatcat>
2024-01-30 15:26:13 +0100

    tag restructuring

--- src/components/ImagePreview.tsx
+++ src/components/ImagePreview.tsx
@@ -2,21 +2,11 @@ import Image from "next/image";
 
 const ImagePreview = ({ previewImage, paddedRaffleNumber }: { previewImage: string, paddedRaffleNumber: string }) => {
     return (
-        <div className="relative mix-blend-lighten w-[120vh] h-[]">
+        <div className="bg-auto bg-no-repeat w-full h-screen" style={{backgroundImage: `url(${previewImage})`}} >
             {previewImage && (
-                <>
-                    <Image 
-                        src={previewImage} 
-                        alt="" 
-                        width={0}
-                        height={0}
-                        sizes="100vw"
-                        className="absolute h-[] w-full object-cover object-right "
-
-                    />
-                    <div className="absolute h-[20vh] w-[30vh] object-cover object-right mx-44 mt-44 sm:mx-72 sm:mt-72" >{paddedRaffleNumber}</div>
-                  
-                </>
+                <div className={`inset-0 flex justify-center z-10`}>  
+                    {paddedRaffleNumber}
+                </div>
             )}
         </div>
     );
